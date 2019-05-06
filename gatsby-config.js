5aaac57873e37a82df64a4dc6a35675536708249
elysia <elysia.hwang@gmail.com>
2019-05-06 00:14:28 -0400

    Add typography plugin and config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Elysia Hwang`,
+    description: `A designer who loves code, or a developer who loves design.`,
+    author: `@elycheea`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
