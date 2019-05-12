a2e8f8722bee3715790b05d3512acf7bc56fe4a1
elysia <elysia.hwang@gmail.com>
2019-05-12 16:12:43 -0400

    Self-host fonts and update in Typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,19 +1,14 @@
 import Typography from "typography";
+import spaceGrotesk from "typeface-spacegrotesk";
+import inconsolata from "typeface-inconsolata";
 
-const typography = new Typography({
-  bodyFontFamily: ['Inconsolata', 'serif']
-});
+const options = {
+  bodyFontFamily: ['Inconsolata', 'serif'],
+  headerFontFamily: ['Space Grotesk', 'sans-serif'],
+};
 
+const typography = new Typography(options);
 // Export helper functions
-export const { scale, rhythm, options } = typography;
-
-typography.googleFonts = [
-  {
-    name: 'Inconsolata',
-    styles: [
-      '400'
-    ]
-  }
-]
+export const { scale, rhythm } = typography;
 
 export default typography;
