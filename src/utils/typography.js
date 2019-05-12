e922d94f83b8b30723267b781d8f434c70ff24fd
elysia <elysia.hwang@gmail.com>
2019-05-12 00:25:33 -0400

    Set Inconsolata as body type.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,19 @@
 import Typography from "typography";
 
-const typography = new Typography();
+const typography = new Typography({
+  bodyFontFamily: ['Inconsolata', 'serif']
+});
 
 // Export helper functions
 export const { scale, rhythm, options } = typography;
+
+typography.googleFonts = [
+  {
+    name: 'Inconsolata',
+    styles: [
+      '400'
+    ]
+  }
+]
+
 export default typography;
