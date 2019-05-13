edeb718095743fd45c7cb8ac5202cd4ed5b75674
elysia <elysia.hwang@gmail.com>
2019-05-12 21:30:10 -0400

    Add siteTitle to index and update layout styles.

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,16 +25,9 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <div className="container">
+      <div className="site-container">
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div className="content-container">
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
