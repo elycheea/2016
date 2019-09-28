bb9c0266b28c6b4f370db75e6682a28a66c36f53
elysia <elysia.hwang@gmail.com>
2019-09-28 12:26:57 -0400

    Move components to subfolders.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,8 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header";
-import "../styles/reset.css"
+import Header from "../site-header/header";
+import "../../styles/reset.css"
 import "./layout.css"
 
 const Layout = ({ children }) => (
